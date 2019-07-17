387d7862c194d04d4ce51fee1c82b80ca437eef0
Zachary Jacobi <zach@alertlabs.com>
2019-07-16 23:32:48 -0400

    Adds in a menu with the following features: - Highlights key sections - Always visible on mobile - Hidden on desktop until you scroll past landing - Required adding no new libraries

---
+++ assets/js/menu.js
@@ -0,0 +1,48 @@
+// TOGGLE HAMBURGER & COLLAPSE NAV
+$('.nav-toggle').on('click', function() {
+	$(this).toggleClass('open');
+	$('.menu-left').toggleClass('collapse');
+});
+
+// REMOVE X & COLLAPSE NAV ON ON CLICK
+$('.menu-left a').on('click', function() {
+	$('.nav-toggle').removeClass('open');
+	$('.menu-left').removeClass('collapse');
+});
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = $(window).height();
+var mobile = $(window).width() <= 568;
+
+$(window).scroll(function(event){
+	didScroll = true;
+});
+
+setInterval(function() {
+	console.log(mobile, $(window).width);
+	mobile = $(window).width() <= 568;
+	if (didScroll && !mobile) {
+		hasScrolled();
+		didScroll = false;
+	} else if (mobile) {
+		$('nav').removeClass('hide-nav-menu').addClass('show-nav-menu');
+	}
+}, 250);
+
+function hasScrolled() {
+	var st = $(this).scrollTop();
+
+	if (st > delta) {
+		// Scroll Down below first load
+		$('nav').removeClass('hide-nav-menu').addClass('show-nav-menu');
+		$('.nav-toggle').removeClass('open');
+		$('.menu-left').removeClass('collapse');
+	} else {
+		// Scroll Up above delta
+		$('nav').removeClass('show-nav-menu').addClass('hide-nav-menu');
+	}
+
+	lastScrollTop = st;
+}
